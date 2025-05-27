48174b36ddde3559d26e57702331aa4cb1d3ca84
João Pedro Silva Cunha <jpcunha5@gmail.com>
2025-05-27 11:50:38 -0300

    :sparkles: Roteador da aplicação até o momento

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { MantineProvider, ColorSchemeScript } from '@mantine/core';
-import { LoginPage } from './pages/Login';
-import { theme } from './theme';
+import { MantineProvider } from '@mantine/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { LoginPage } from './pages/Login/LoginPage';
+import { RegisterPage } from './pages/Register/RegisterPage';
+import { HomePage } from './pages/Home/HomePage';
+
+import { theme } from './theme';
 import '@mantine/core/styles.css';
 
 export default function App() {
   return (
-    <>
-      <ColorSchemeScript />
-      <MantineProvider
-        theme={theme}
-        defaultColorScheme="light"
-      >
-        <LoginPage />
-      </MantineProvider>
-    </>
+    <MantineProvider theme={theme} defaultColorScheme="light">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   );
 }
